eefa562cf67069594012eecfc2ee7a10db8a52fe
Lionel <lionelngolo.pro@gmail.com>
2022-01-21 17:12:50 +0100

    Tp netflix avec les microservices

---
+++ TP_netflix/frontWebService/frontWebService/src/test/java/com/example/frontWebService/FrontWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.frontWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
